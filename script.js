71fc2394dd31f5913e8308a7c1d2d3858ab7320a
Uday 5D6 <uday5d6@gmail.com>
2025-11-24 12:12:15 +0530

    Update index.html and script.js

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
+// Get element by ID
 const title = document.getElementById("title");
 console.log("By ID:", title);
 
+// Get element by class name
 const intros = document.getElementsByClassName("into");
 console.log("By Class:", intros);
 
+// Get element by tag name 
 const paragraphs = document.getElementsByTagName("p");
 console.log("by Tag", paragraphs);
 
+// querySelector (first name)
 const firstItem = document.querySelector(".item");
 console.log("Query Selector:", firstItem);
 
+// querySelectorAll (all matches) 
 const allItems = document.querySelectorAll(".item");
 console.log("Query selector all:", allItems);
 
+// complex selector
 const specialItem = document.querySelector("#list .item.special");
 console.log('Complex selector:', specialItem);
+
+const byClass = document.getElementsByClassName('item'); // HTML Collection
+const byQuery = document.querySelectorAll('.item'); // NodeList
+
+console.log('Is Array?', Array.isArray(byClass));  // false
+console.log('Has forEach?', typeof byQuery.forEach);  // function 
+
+// Convert to array
+const itemsArray = Array.from(byClass);
 
 const heading = document.getElementById('heading');
 const paragraph = document.getElementById('paragraph');
 const input = document.getElementById('input');
 const output = document.getElementById('output'); 
 
-document.getElementById('btn-html').addEventListener('click', () => {
+document.getElementById('btn-text').addEventListener('click', () => {
     heading.textContent = 'New Heading Text';
 });  
 
@@ -30,11 +45,15 @@ document.getElementById('btn-html').addEventListener('click', () => {
 }); 
 
 document.getElementById('btn-class').addEventListener('click', () => {
-    heading.style.color = 'red'; 
-    heading.style.fontSize = '32px';
+    paragraph.classList.toggle('highlight');
 }); 
 
-document.getElementById(btn-attr).addEventListener('click', () => {
+document.getElementById('btn-style').addEventListener('click', () => {
+    heading.style.color = 'red'; 
+    heading.style.fontSize = '32px';
+})
+
+document.getElementById('btn-attr').addEventListener('click', () => {
     input.setAttribute('placeholder', 'New placeholder');
     input.value = 'New value';
 }); 
@@ -49,7 +68,7 @@ console.log('Has class?', paragraph.classList.contains('normal'));
 
 console.log('Color:', heading.style.color); 
 
-const user = document.getElementsById('user'); 
+const user = document.getElementById('user'); 
 console.log(user.dataset.userId); 
 console.log(user.dataset.role); 
 user.dataset.status = 'active'; 
